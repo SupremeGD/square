9636548a1634ac6577d5e26f8ee3af9df353418a
SupremeGD <759296837@qq.com>
2022-07-23 23:10:26 +0800

    提交文件

---
+++ js/square.js
@@ -0,0 +1,53 @@
+var div = document.querySelector("#square");
+var btn1 = document.querySelector("#button_1");
+var btn2 = document.querySelector("#button_2");
+var btn3 = document.querySelector("#button_3");
+var btn4 = document.querySelector("#button_4");
+var btn5 = document.querySelector("#button_5");
+
+//按钮1
+var sum1 = 0;
+btn1.onclick = function button_1() {
+    sum1 = sum1 + 1;
+    if (sum1 % 2 == 0) {
+        div.style.width = "100px";
+    } else {
+        div.style.width = "200px";
+    }
+}
+
+//按钮2
+var sum2 = 0;
+btn2.onclick = function button_2() {
+    sum2 = sum2 + 1;
+    if (sum2 % 2 == 0) {
+        div.style.height = "100px";
+    } else {
+        div.style.height = "200px";
+    }
+}
+
+//按钮3
+var sum3 = 0;
+btn3.onclick = function button_3() {
+    sum3 = sum3 + 1;
+    if (sum3 % 2 == 0) {
+        div.style.background = "greenyellow";
+    } else {
+        div.style.background = "red";
+    }
+}
+
+//按钮4
+var right = 200;
+btn4.onclick = function button_4() {
+    div.style.transform = "translateX(" + right + "px)";
+    right = parseInt(right + 200);
+}
+
+//按钮5
+var left = parseInt(btn4.right- 200);
+btn5.onclick = function button_5() {
+    div.style.transform = "translateX(" + left + "px)";
+    left = parseInt(left - 200);
+}
